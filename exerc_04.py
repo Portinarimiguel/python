de3bcca8033154681243b06216c4fcf953124f0a
Portinarimiguel <portinarimigueldeoliveira@gmail.com>
2024-08-28 18:38:10 -0300

    Create exerc_04.py

---
+++ exerc_04.py
@@ -0,0 +1,8 @@
+numero1 = float(input("digite o primeiro numero: "))
+numero2 = float(input("digite o segundo numero: "))
+
+if numero1 > numero2:
+   maior = numero1
+else:
+    maior = numero2
+    print("o maior número é: ", maior)
