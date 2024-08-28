8e1af1522a1ff476af1c1c4143cb24a964c5e954
Portinarimiguel <portinarimigueldeoliveira@gmail.com>
2024-08-28 18:37:17 -0300

    Create exerc_03.py

---
+++ exerc_03.py
@@ -0,0 +1,7 @@
+numero1 = float(input("digite o primeiro numero: "))
+numero2 = float(input("digite o segundo numero: "))
+numero3 = float(input("digite o terceiro numero: "))
+
+soma = numero1 + numero2 + numero3
+
+print("a soma dos numeros é: " , soma)
